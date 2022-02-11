5528c6b346c4f84cb7292ba417215987d4c2fd01
maxfahl <max@pixilab.se>
2022-02-11 21:33:34 +0100

    Requests model.

---
+++ backend/api/models/request.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Request(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    # category = models.ForeignKey(
+    #     Category, related_name="ingredients", on_delete=models.CASCADE
+    # )
+
+    def __str__(self):
+        return self.title
